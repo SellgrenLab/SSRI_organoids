2ed3ce2c5ddb7722f754545f8f0e90d5f2ea2039
Susmita Malwade <56251389+sm2695@users.noreply.github.com>
2024-12-18 15:00:48 +0100

    Create disorder_enrich.R

---
+++ scripts/disorder_enrich.R
@@ -0,0 +1,48 @@
+library(GeneOverlap)
+library(ggplot2)
+library(dplyr)
+
+Geneset_with_disease_riskgenes <- readRDS("/Volumes/projects/C3_Sellgren_lab/Lab Members/Susmita/Internal data/C1Q/data/Geneset_with_disease_riskgenes.rds")
+Trubetskoy_scz_genes <- readRDS("/Volumes/projects/C3_Sellgren_lab/Lab Members/Susmita/Internal data/C1Q/Bipolar/Trubetskoy_scz_genes.rds")
+adhd <- c("PTPRF", "KDM4A", "SNRK", "ANO10", "ABHD5", "TRAIP", "CAMKV", "MST1R", "MON1A", "IQCF3", "IQCF2", "IQCF5", "IQCF1", "FOXP1", "VGLL3", "LSM6", "SLC10A7", "COL19A1", "FOXP2", "ARHGAP39", "SORCS3", "METTL15", "DUSP6", "POC1B", "CDH8", "TMEM200C", "DCC", "XRN2", "NKX2-4")
+asd<-  readRDS("/Volumes/projects/C3_Sellgren_lab/Lab Members/Susmita/Internal data/C1Q/Bipolar/ASD_riskgenes.rds")
+adhd<-  readRDS("/Volumes/projects/C3_Sellgren_lab/Lab Members/Susmita/Internal data/C1Q/Bipolar/ADHD_riskgenes.rds")
+mdd<- read.xlsx("/Users/susmita.malwade/MDD.xlsx")
+
+
+#get genome size from sc dataset (all features that were tested) and test the overlap statistically
+
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = genes, listB = asd))) ## New asd genes with de novo variants as well
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = genes, listB = Geneset_with_disease_riskgenes$ASD_Satterstrom$Gene)))
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = genes, listB = Geneset_with_disease_riskgenes$ASD_Ruzzo$`HGNC gene symbol`)))
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = genes, listB = Geneset_with_disease_riskgenes$ASD_Sanders$RefSeqGeneName)))
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = genes, listB = Geneset_with_disease_riskgenes$DDD$symbol)))
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = genes, listB = adhd)))
+
+
+## Create a data frame for plotting
+df<- data.frame("Disorder" = c("ASD(Cirnigliaro)", "ASD(Satterstom)", "ASD(Ruzzo)", "ASD (Sanders)", "DDD", "ADHD", "MDD"))
+df$pval <- c(2.4e-03, 0.31,0.025, 1.6e-03,0.019,1, 0.25)
+df$OR <- c(12.1, 2.8, 8.5, 13.9, 4.1, 0, 2.1)
+df$padj <- p.adjust(df$pval, method = "BH")
+df$plot <- -log10(df$padj)
+## Plot the results
+
+plot1<- ggplot(data= df, mapping = aes(x=plot, y=Disorder, fill=OR)) + geom_bar(stat="identity") + scale_fill_viridis() + xlab(label = "-log(p-adjusted)") + theme_classic() + geom_vline(xintercept = -log10(0.05)) + ggtitle(label = "Disorder risk enrichment in PseudoDEG")
+
+
+### DEGs
+results_de <- read.xlsx("./sertraline/201024_SSRI_pseudobulk_results_all.xlsx")
+results_df_sig <- dplyr::filter(results_de, padj < 0.05)
+
+df1<- data.frame("Disorder" = c("ASD(Cirnigliaro)", "ASD(Satterstom)", "ASD(Ruzzo)", "ASD (Sanders)", "DDD", "ADHD", "MDD"))
+df1$pval <- c(1.1e-05,8.6e-05,7.4e-05,5.3e-05,8.3e-06,0.26,1.9e-03)
+df1$OR <- c(10.4, 7.3,9.4, 10.1,4.8,3.5, 3.4)
+df1$padj <- p.adjust(df1$pval, method = "BH")
+df1$plot <- -log10(df1$padj)
+## Plot the results
+
+plot2<- ggplot(data= df1, mapping = aes(x=plot, y=Disorder, fill=OR)) + geom_bar(stat="identity") + scale_fill_viridis() + xlab(label = "-log(p-adjusted)") + theme_classic() + geom_vline(xintercept = -log10(0.05)) + ggtitle(label = "Disorder risk enrichment in DEGs")
+
+
+cowplot::plot_grid(plot1,plot2,ncol = 1)
