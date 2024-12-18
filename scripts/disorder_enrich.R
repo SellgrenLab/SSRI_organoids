21c4807e2e4bc705349b1512595b53c10f721068
Susmita Malwade <56251389+sm2695@users.noreply.github.com>
2024-12-18 15:06:14 +0100

    Update disorder_enrich.R

--- scripts/disorder_enrich.R
+++ scripts/disorder_enrich.R
@@ -8,6 +8,10 @@ asd<-  readRDS("/Volumes/projects/C3_Sellgren_lab/Lab Members/Susmita/Internal d
 adhd<-  readRDS("/Volumes/projects/C3_Sellgren_lab/Lab Members/Susmita/Internal data/C1Q/Bipolar/ADHD_riskgenes.rds") #demontis et al
 mdd<- read.xlsx("/Users/susmita.malwade/MDD.xlsx") #https://www-nature-com.proxy.kib.ki.se/articles/s41588-023-01596-4
 
+## Read in the pseudo time-related DEGenes
+pde <- read.xlsx("./sertraline/Results/Sertraline_PseudoDE_Results.xlsx")
+pde<- dplyr::filter(pde, padj<0.01)
+genes <- pde$gene
 
 #get genome size from sc dataset (all features that were tested) and test the overlap statistically
 
@@ -31,14 +35,25 @@ plot1<- ggplot(data= df, mapping = aes(x=plot, y=Disorder, fill=OR)) + geom_bar(
 
 
 ### DEGs
+## read in the celltype-specific DEGs
 results_de <- read.xlsx("./sertraline/201024_SSRI_pseudobulk_results_all.xlsx")
 results_df_sig <- dplyr::filter(results_de, padj < 0.05)
+
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = unique(results_df_sig$feature), listB = mdd$Gene.Name)))
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = unique(results_df_sig$feature), listB = adhd)))
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = unique(results_df_sig$feature), listB = gene_vector)))
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = unique(results_df_sig$feature), listB = Geneset_with_disease_riskgenes$ASD_Ruzzo$`HGNC gene symbol`)))
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = unique(results_df_sig$feature), listB = Geneset_with_disease_riskgenes$ASD_Sanders$RefSeqGeneName)))
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = unique(results_df_sig$feature), listB = Geneset_with_disease_riskgenes$ASD_Satterstrom$Gene)))
+print(testGeneOverlap(newGeneOverlap(genome.size = 29458, listA = unique(results_df_sig$feature), listB = Geneset_with_disease_riskgenes$DDD$symbol)))
+
 
 df1<- data.frame("Disorder" = c("ASD(Cirnigliaro)", "ASD(Satterstom)", "ASD(Ruzzo)", "ASD (Sanders)", "DDD", "ADHD", "MDD"))
 df1$pval <- c(1.1e-05,8.6e-05,7.4e-05,5.3e-05,8.3e-06,0.26,1.9e-03)
 df1$OR <- c(10.4, 7.3,9.4, 10.1,4.8,3.5, 3.4)
 df1$padj <- p.adjust(df1$pval, method = "BH")
 df1$plot <- -log10(df1$padj)
+
 ## Plot the results
 
 plot2<- ggplot(data= df1, mapping = aes(x=plot, y=Disorder, fill=OR)) + geom_bar(stat="identity") + scale_fill_viridis() + xlab(label = "-log(p-adjusted)") + theme_classic() + geom_vline(xintercept = -log10(0.05)) + ggtitle(label = "Disorder risk enrichment in DEGs")
